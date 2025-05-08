c7b85cf0bf87c623caceb3190d37114964a31b8f
Dre Suguitan <ddre2026@gmail.com>
2025-05-08 16:52:28 -0700

    lab 8

---
+++ packages/proto/src/scripts/todo-index.ts
@@ -0,0 +1,72 @@
+import { html, css, LitElement } from "lit";
+import { property } from "lit/decorators.js";
+import reset from "./styles/reset.css.ts";
+
+export class TaskToDoElement extends LitElement {
+  @property() href?: string = '';
+  @property() title: string = 'Default Title';
+  @property({ attribute: 'svg-src' }) svgSrc?: string;
+
+  override render() {
+    return html`
+      <a href=${this.href} class="task-todo-link">
+          <div class="task-todo">
+            <div class="task-todo-icon">${this.svgSrc}</div>
+            <div class="task-todo-title">${this.title}</div>
+            <slot="action"><button class="task-todo-edit-btn" onclick="event.stopPropagation()">Done?</button></slot>
+          </div>
+      </a>
+    `;
+  }
+
+  static styles = [
+    reset.styles,
+    css`
+      .task-todo-link {
+          text-decoration: none; 
+          color: inherit;         
+          display: block;
+          width: 100%;
+      }
+      
+      .task-todo {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          color: var(--color-text);
+          background-color: var(--color-task-todo-background);
+          padding: 12px 16px;
+          border-radius: 8px;
+          margin-bottom: 16px;
+          background-color 0.3s ease, color 0.3s ease;
+      }
+      
+      .task-todo:hover {
+          font-weight: bold;
+          background-color: var(--color-task-todo-background-hover);
+      }
+      
+      .task-todo-icon {
+          font-size: 24px;
+          font-weight: bold;
+          margin-right: 12px;
+      }
+
+      .task-todo-title {
+          flex-grow: 1;
+          font-family: var(--font-display);
+          font-size: 25px;
+          /* font-weight: bold; */
+          text-align: left;
+      }
+      
+      .task-todo-edit-btn {
+          background-color: #C8BEA4;
+          border: 1px solid var(--color-border-searchbar);
+          padding: 4px 12px;
+          border-radius: 24px;
+          cursor: pointer;
+          font-size: 14px;
+      }
+    `];
+}
