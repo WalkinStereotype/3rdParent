75b1532ec75eefdfe15571d62d600f0459ca5155
Franklin The Turtle <ddre2026@gmail.com>
2025-11-20 13:30:00 -0800

    Basic UI for To-do

--- src/views/Todo.tsx
+++ src/views/Todo.tsx
@@ -1,3 +1,17 @@
-export default function ToDo() {
-  return <p>ToDo!</p>;
+import SkillList from "@/components/skills/SkillList";
+
+import { useSkills } from "@/hooks/contexts/useSkills";
+
+export default function Todos() {
+  const { skills, reload_skills } = useSkills();
+  return (
+    <div>
+      <h1>To-do</h1>
+      <br />
+      <SkillList skills={skills.slice(0, 3)} />
+      <h1>Backlog</h1>
+      <br />
+      <SkillList skills={skills.slice(3, 10)} />
+    </div>
+  );
 }
