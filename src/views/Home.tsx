a6e86dd3176c45ca5c02393cc0276101fb769e60
Franklin The Turtle <ddre2026@gmail.com>
2025-12-04 13:49:16 -0800

    Lists recent logs in home page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,19 +1,20 @@
 import SkillsSection from "@/components/shared/SkillsSection";
 import { useNavigate } from "react-router-dom";
 
-import StarButton from "@/components/shared/skill-buttons/StarButton";
 import RemoveButton from "@/components/shared/skill-buttons/RemoveButton";
 
-import { useSkills, useTodos } from "@/hooks/contexts";
+import { useSkills, useLogs, useTodos } from "@/hooks/contexts";
 import { Skill } from "@/utils/schema";
 import DoneButton from "@/components/shared/skill-buttons/DoneButton";
 
 export default function Home() {
   const navigate = useNavigate();
   const { skills, reload_skills, loading: skillsLoading } = useSkills();
+  const { logs, reload_logs, loading: logsLoading } = useLogs();
   const { todos, reload_todos, loading: todosLoading } = useTodos();
 
-  const homePageLoading = skillsLoading || todosLoading || skills.length == 0;
+  const homePageLoading =
+    skillsLoading || logsLoading || todosLoading || skills.length == 0;
 
   const todoSkills = homePageLoading
     ? []
@@ -21,7 +22,13 @@ export default function Home() {
         .filter(({ is_priority }) => is_priority)
         .map(({ skill_id }) => skills.find(({ id }) => id === skill_id)!);
 
-  const renderActions = (s: Skill) => (
+  const recentLogSkills = homePageLoading
+    ? []
+    : logs
+        .slice(0, 3)
+        .map(({ skill_id }) => skills.find(({ id }) => id === skill_id)!);
+
+  const renderInProgressActions = (s: Skill) => (
     <div className="flex-display">
       <DoneButton onClick={() => navigate(`/skills/${s.id}?write=true`)} />
     </div>
@@ -32,8 +39,14 @@ export default function Home() {
       <SkillsSection
         title={"In Progress..."}
         skills={todoSkills}
-        renderActions={(s) => renderActions(s)}
+        renderActions={(s) => renderInProgressActions(s)}
         emptyText="Find skills to do in the Skills page!"
+        loading={homePageLoading}
+      />
+      <SkillsSection
+        title={"Recent Accomplishments"}
+        skills={recentLogSkills}
+        emptyText="Nothing learned yet, get started right now!"
         loading={homePageLoading}
       />
     </div>
